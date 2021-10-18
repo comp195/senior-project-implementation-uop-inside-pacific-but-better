c2c1fea7ddffa7ab877f3fae020d8a7e274bcb11
Prot0ast <a_barragan1@u.pacific.edu>
2021-10-17 20:39:28 -0700

    Adding menu navigation between the two different prototypes

---
+++ Assets/Scripts/PrototypeSelector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrototypeSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectOne()
+    {
+        SceneManager.LoadScene(sceneName: "Prototype1");
+    }
+
+    public void SelectTwo()
+    {
+        SceneManager.LoadScene(sceneName: "Prototype2");
+    }
+}
