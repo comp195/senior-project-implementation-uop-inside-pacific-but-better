29f6a3d3b222642358dbc8f4cb3e16a23d6f7d8d
Phorus <c_fines@u.pacific.edu>
2021-10-28 19:08:44 -0700

    Added further pages within Prototype2.1 Academic Page

--- Assets/Scripts/Prototype2AcademicPageTabs.cs
+++ Assets/Scripts/Prototype2AcademicPageTabs.cs
@@ -6,9 +6,12 @@ using UnityEngine.EventSystems;
 public class Prototype2AcademicPageTabs : MonoBehaviour
 {
     public GameObject page1, page2, page3, page4, page5, page6, page7, page8, page9;
-    public GameObject parent, currentSelectedGameObject;
+    public GameObject parent, AcademicServicesParent, currentSelectedGameObject;
     public Dictionary<string, GameObject> dict = new Dictionary<string, GameObject>();
     public bool updateDict = true;
+
+    public Dictionary<string, GameObject> AcademicServicesDict = new Dictionary<string, GameObject>();
+    public GameObject AcademicServicesPage1, AcademicServicesPage2, AcademicServicesPage3;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +19,7 @@ public class Prototype2AcademicPageTabs : MonoBehaviour
         if (updateDict == true)
         {
             updateDict = false;
+            // For the pages that correspond to the top most row of buttons on the Academic page
             dict.Add("Academic Services Button", page1);
             dict.Add("Manage Your Account Button", page2);
             dict.Add("Financial Aid Links Button", page3);
@@ -25,11 +29,18 @@ public class Prototype2AcademicPageTabs : MonoBehaviour
             dict.Add("Follett Bookstore Button", page7);
             dict.Add("Course Syllabi Repository Button", page8);
             dict.Add("LinkedIn Learning Button", page9);
+            // For the pages that correspond to the academic services button
+            AcademicServicesDict.Add("Personal Information Button", AcademicServicesPage1);
+            AcademicServicesDict.Add("Financial Aid Button", AcademicServicesPage2);
+            AcademicServicesDict.Add("Student Services Button", AcademicServicesPage3);
         }
     }
 
+    // REMEMBER TO RELACE THE DICTIONARY AND PARENT WHEN DUPLICATING FUNCTIONS
     public void loopChildren()
     {
+        // Loops through all children within the parent the button was pressed.
+        // Hide every page that's not corresponding to the button (specified in the dictionary declatations).
         for (int i = 0; i < parent.transform.childCount; i++)
         {
             //Debug.Log(parent.transform.GetChild(i).name);
@@ -48,6 +59,32 @@ public class Prototype2AcademicPageTabs : MonoBehaviour
             else if (converted != temp.transform.name)
             {
                 parent.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void loopChildrenAcademicServices()
+    {
+        // Loops through all children within the parent the button was pressed.
+        // Hide every page that's not corresponding to the button (specified in the dictionary declatations).
+        for (int i = 0; i < AcademicServicesParent.transform.childCount; i++)
+        {
+            //Debug.Log(parent.transform.GetChild(i).name);
+            string converted = AcademicServicesParent.transform.GetChild(i).name;
+            string name = EventSystem.current.currentSelectedGameObject.name;
+            //Debug.Log(dict.ContainsKey(name));
+            AcademicServicesDict.TryGetValue(name, out GameObject temp);
+
+            //Debug.Log(converted);
+            //Debug.Log(temp.transform.name);
+
+            if (converted == temp.transform.name)
+            {
+                temp.SetActive(true);
+            }
+            else if (converted != temp.transform.name)
+            {
+                AcademicServicesParent.transform.GetChild(i).gameObject.SetActive(false);
             }
         }
     }
