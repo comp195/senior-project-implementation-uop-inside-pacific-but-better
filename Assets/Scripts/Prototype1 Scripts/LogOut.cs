04052bd23c8ea5026502373a6b6e105d0f4c5c7d
Phorus <c_fines@u.pacific.edu>
2021-10-28 20:41:15 -0700

    Completed functionality for Academic Services tab

--- Assets/Scripts/Prototype1 Scripts/LogOut.cs
+++ Assets/Scripts/Prototype1 Scripts/LogOut.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class LogOut : MonoBehaviour
 {
-    [SerializeField] private GameObject logOutButton;
+    [SerializeField] private GameObject logOutButton, Prototype2DashBoardButton;
     // Start is called before the first frame update
 
     public void LogOutofIPBB()
     {
         SceneManager.LoadScene(sceneName: "MainSelection");
+    }
+
+    // Prototype 1 Functions
+
+    // Prototype 2 Functions
+    public void Prototype2Dashboard()
+    {
+        SceneManager.LoadScene(sceneName: "Prototype2.1");
+    }
+    public void Prototype2Academic()
+    {
+        SceneManager.LoadScene(sceneName: "Prototype2.1-Academic");
     }
 }
