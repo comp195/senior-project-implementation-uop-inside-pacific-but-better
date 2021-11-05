c98d1accf1fea9ea921e70a77799843634422aaa
Prot0ast <a_barragan1@u.pacific.edu>
2021-11-04 21:56:36 -0700

    Adjusted spawning location of tabs

--- Assets/Scripts/Prototype1 Scripts/TabGenerator.cs
+++ Assets/Scripts/Prototype1 Scripts/TabGenerator.cs
@@ -19,8 +19,8 @@ public class TabGenerator : MonoBehaviour
     public Vector2 RandomCords()
     {
         Vector2 tabPos = transform.position;
-        tabPos.x = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
-        tabPos.y = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+        tabPos.x = Random.Range(90,110);
+        tabPos.y = Random.Range(90,110);
         return tabPos;
     }
 }
